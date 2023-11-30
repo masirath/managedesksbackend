41fd51312bfa2cbccaa8d8f69389f3916bff6a35
Rihan <mohammedrihanmasirath@gmail.com>
2023-11-30 08:44:06 +0400

    port check

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.use(express.json());
 // app.use(branch);
 
 app.get("/", (req, res) => {
-  res.send(`<h1>Working</h1> ${port} == 4000`);
+  res.send(`<h1>Working</h1> ${port} == 4000 Port`);
 });
 
 app.listen(port, () => {
