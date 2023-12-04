16e4ca71295a8a112e80ca63a6009cc71a1917a7
Rihan <mohammedrihanmasirath@gmail.com>
2023-12-04 16:31:20 +0400

    user crud

--- Routes/users.js
+++ Routes/users.js
@@ -5,13 +5,15 @@ const {
   create_user,
   update_user,
   get_user,
+  get_all_user,
   verify_user,
 } = require("../Controllers/users");
 
 users.post("/create-account", create_account);
 users.post("/create-user", create_user);
 users.post("/update-user", update_user);
-users.get("/user-get/:id", get_user);
+users.get("/get-user/:id", get_user);
+users.get("/get-all-users", get_all_user);
 users.post("/verify-user", verify_user);
 
 module.exports = users;
