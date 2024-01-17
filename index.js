b8ecd56dceb53fdad26d7db7f4af77795f4d92ca
Rihan <mohammedrihanmasirath@gmail.com>
2024-01-17 09:03:34 +0400

    development api

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const mongoose = require("mongoose");
-const port = process.env.PRODUCTION_PORT;
-const mongodb = process.env.PRODUCTION_DATABASE;
+const port = process.env.DEVELOPMENT_PORT;
+const mongodb = process.env.DEVELOPMENT_DATABASE;
 
 mongoose.connect(mongodb);
 const database = mongoose.connection;
